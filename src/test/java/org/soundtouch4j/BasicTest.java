1b020f77a6c6cf3dd66b1c1136b3f671c26bb35b
Gerald Madlmayr <gerald.madlmayr@gmx.at>
2018-09-10 20:37:59 +0200

    Adding SsdpScanner

--- src/test/java/org/soundtouch4j/BasicTest.java
+++ src/test/java/org/soundtouch4j/BasicTest.java
@@ -1,18 +1,24 @@
 package org.soundtouch4j;
 
-
+import java.util.List;
 import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import io.resourcepool.ssdp.model.SsdpService;
 
 public class BasicTest {
 
-
-  private static Logger LOGGER = LoggerFactory.getLogger(BasicTest.class);
+  private static final Logger LOGGER = LoggerFactory.getLogger(BasicTest.class);
 
   @Test
   public void test01_basicTestWithBox() {
-    LOGGER.info("Hello World");
+
+    final List<SsdpService> services = SsdpScanner.synchronousBlockingDeviceScaner(5000);
+
+    for (final SsdpService service : services) {
+      LOGGER.info("Service '{}' with IP '{}' found", service.getSerialNumber(), service.getRemoteIp());
+    }
+
   }
 
 }
