291ec48545424940fd817015960e32fd57ffe6c7
Gerald Madlmayr <gerald.madlmayr@gmx.at>
2018-09-10 20:20:40 +0200

    Adding Log4j/SLF4J Logging

---
+++ src/test/java/org/soundtouch4j/BasicTest.java
@@ -0,0 +1,18 @@
+package org.soundtouch4j;
+
+
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class BasicTest {
+
+
+  private static Logger LOGGER = LoggerFactory.getLogger(BasicTest.class);
+
+  @Test
+  public void test01_basicTestWithBox() {
+    LOGGER.info("Hello World");
+  }
+
+}
