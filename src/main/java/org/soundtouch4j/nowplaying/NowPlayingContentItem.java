c2e29dd50e55d075910e51247723b5529ca970aa
Gerald Madlmayr <gerald.madlmayr@gmx.at>
2018-09-12 20:31:45 +0200

    Update ContentItem PoJo

--- src/main/java/org/soundtouch4j/nowplaying/NowPlayingContentItem.java
+++ src/main/java/org/soundtouch4j/nowplaying/NowPlayingContentItem.java
@@ -11,11 +11,46 @@ public class NowPlayingContentItem {
   @Key("@source")
   private SourceEnum source;
 
+  @Key("@isPresetable")
+  private boolean isPresetable;
+
+  @Key("@location")
+  private String location;
+
+  @Key("@sourceAccount")
+  private String sourceAccount;
+
+  @Key
+  private String itemName;
+
+  @Key
+  private String containerArt;
+
   public NowPlayingContentItem() {
 
   }
 
   public SourceEnum getSource() {
     return source;
+  }
+
+  public boolean isPresetable() {
+    return isPresetable;
+  }
+
+  public String getLocation() {
+    return location;
+  }
+
+  public String getSourceAccount() {
+    return sourceAccount;
+  }
+
+  public String getItemName() {
+    return itemName;
+  }
+
+  public String getContainerArt() {
+    return containerArt;
   }
 }
