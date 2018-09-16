c7434c8a81311a5565087b5b075a252452d0a710
Gerald Madlmayr <gerald.madlmayr@gmx.at>
2018-09-16 17:30:18 +0200

    Add method for fetching sources by type

--- src/main/java/org/soundtouch4j/source/SourceApi.java
+++ src/main/java/org/soundtouch4j/source/SourceApi.java
@@ -1,8 +1,11 @@
 package org.soundtouch4j.source;
 
+import java.util.List;
+import java.util.stream.Collectors;
 import org.soundtouch4j.SoundTouchApi;
 import org.soundtouch4j.SoundTouchApiException;
 import org.soundtouch4j.common.AbstractApi;
+import org.soundtouch4j.common.SourceEnum;
 
 public class SourceApi extends AbstractApi {
 
@@ -25,4 +28,21 @@ public class SourceApi extends AbstractApi {
         .get(PATH_FOR_API, SourceResponse.class);
 
   }
+
+  /**
+   * Method of fetch only sources of a dedicated {@link SourceEnum}
+   *
+   * @param type Type of Sources to filter for
+   * @return List of {@link SourceItem} with the given type
+   * @throws SoundTouchApiException is thrown in case the communication to the speaker failed or the Speaker did response
+   */
+
+  public List<SourceItem> getSourcesByType(final SourceEnum type) throws SoundTouchApiException {
+    return getSources().getSourceItems()
+        .stream()
+        .filter(s -> s.getSource()
+            .equals(type))
+        .collect(Collectors.toList());
+  }
+
 }
