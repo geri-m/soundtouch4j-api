ca2841107053bf50d0dc5479914d65b79fa7bd30
Gerald Madlmayr <gerald.madlmayr@gmx.at>
2018-09-27 21:28:25 +0200

    Update GroupApi and add Tests (#18)

--- src/main/java/org/soundtouch4j/group/GroupApi.java
+++ src/main/java/org/soundtouch4j/group/GroupApi.java
@@ -3,10 +3,14 @@ package org.soundtouch4j.group;
 import org.soundtouch4j.SoundTouchApi;
 import org.soundtouch4j.SoundTouchApiException;
 import org.soundtouch4j.common.AbstractApi;
+import org.soundtouch4j.info.InfoResponse;
 
 public class GroupApi extends AbstractApi {
 
   private static final String PATH_FOR_API = "getGroup";
+  private static final String PATH_FOR_INFO = "info";
+  private static final String ST10 = "SoundTouch 10";
+  private static final int MIN_VERSION_FOR_GROUP = 14;
 
   public GroupApi(final SoundTouchApi soundTouchApi) {
     super(soundTouchApi);
@@ -21,6 +25,28 @@ public class GroupApi extends AbstractApi {
    */
 
   public Group getGroup() throws SoundTouchApiException {
+
+    final InfoResponse infoResponse = soundTouchApi.getSoundTouchApiClient()
+        .get(PATH_FOR_INFO, InfoResponse.class);
+
+    if (infoResponse.getComponents()
+        .isEmpty()) {
+      throw new SoundTouchApiException("Unable to fetch Version of SoundTouch");
+    }
+
+    if (infoResponse.getComponents()
+        .get(0)
+        .getVersion() <= MIN_VERSION_FOR_GROUP) {
+      throw new SoundTouchApiException(String.format("Major Version of Firmware is '%s', but needs to be > 14", infoResponse.getComponents()
+          .get(0)
+          .getVersion()));
+    }
+
+    if (!infoResponse.getType()
+        .equals(ST10)) {
+      throw new SoundTouchApiException(String.format("/group Call Requires SoundTouch 10, but this is '%s'", infoResponse.getType()));
+    }
+
     return soundTouchApi.getSoundTouchApiClient()
         .get(PATH_FOR_API, Group.class);
   }
--- src/test/java/org/soundtouch4j/GroupTest.java
+++ src/test/java/org/soundtouch4j/GroupTest.java
@@ -19,22 +19,100 @@ public class GroupTest extends TestCase {
 
   private static final Logger LOGGER = LoggerFactory.getLogger(GroupTest.class);
 
+  private static final MockLowLevelHttpRequest GROUP_MOCK_OK = new MockLowLevelHttpRequest() {
+    @Override
+    public LowLevelHttpResponse execute() {
+      final MockLowLevelHttpResponse result = new MockLowLevelHttpResponse();
+      result.setContentType(Xml.MEDIA_TYPE);
+      result.setContent("<?xml version=\"1.0\" encoding=\"UTF-8\" ?><group id=\"$STRING\"><name>name</name><masterDeviceId>masterDeviceId</masterDeviceId><roles><groupRole" +
+          "><deviceId>deviceId1</deviceId><role>LEFT1</role><ipAddress>ipAddress1</ipAddress></groupRole><groupRole><deviceId>deviceId2</deviceId><role>RIGHT2</role" +
+          "><ipAddress>ipAddress2</ipAddress></groupRole></roles><senderIPAddress>senderIPAddress</senderIPAddress><status>status</status></group>");
+      return result;
+    }
+  };
+
+  private static final MockLowLevelHttpRequest GROUP_MOCK_EMPTY = new MockLowLevelHttpRequest() {
+    @Override
+    public LowLevelHttpResponse execute() {
+      final MockLowLevelHttpResponse result = new MockLowLevelHttpResponse();
+      result.setContentType(Xml.MEDIA_TYPE);
+      result.setContent("<?xml version=\"1.0\" encoding=\"UTF-8\" ?><group id=\"$STRING\"><name>name</name><masterDeviceId>masterDeviceId</masterDeviceId><roles" +
+          "/><senderIPAddress>senderIPAddress</senderIPAddress><status>status</status></group>");
+      return result;
+    }
+  };
+
+  private static final MockLowLevelHttpRequest INFO_MOCK_OK = new MockLowLevelHttpRequest() {
+    @Override
+    public LowLevelHttpResponse execute() {
+      final MockLowLevelHttpResponse result = new MockLowLevelHttpResponse();
+      result.setContentType(Xml.MEDIA_TYPE);
+      result.setContent("<?xml version=\"1.0\" encoding=\"UTF-8\" ?><info deviceID=\"C8DF84AE0B6E\"><name>SoundTouch 10</name><type>SoundTouch 10</type" + "><margeAccountUUID" +
+          ">6990307</margeAccountUUID><components><component><componentCategory>SCM</componentCategory><softwareVersion>19.0.5.42017.2794643 " + "epdbuild.trunk.cepeswbld02" +
+          ".2018-04-25T18:23:30</softwareVersion><serialNumber>F8124895404720048620440</serialNumber></component><component><componentCategory>" + "PackagedProduct" +
+          "</componentCategory><serialNumber>069428P81639976AE</serialNumber></component></components><margeURL>https://streaming.bose.com</margeURL>" + "<networkInfo " +
+          "type=\"SCM\"><macAddress>C8DF84AE0B6E</macAddress><ipAddress>192.168.178.61</ipAddress></networkInfo><networkInfo " + "type=\"SMSC\"><macAddress>C8DF84615084" +
+          "</macAddress><ipAddress>192.168.178" + ".61</ipAddress></networkInfo><moduleType>sm2</moduleType><variant>spotty</variant><variantMode>normal</variantMode" +
+          "><countryCode>GB</countryCode><regionCode>GB" + "</regionCode></info>");
+      return result;
+    }
+  };
+
+  private static final MockLowLevelHttpRequest INFO_MOCK_ST20 = new MockLowLevelHttpRequest() {
+    @Override
+    public LowLevelHttpResponse execute() {
+      final MockLowLevelHttpResponse result = new MockLowLevelHttpResponse();
+      result.setContentType(Xml.MEDIA_TYPE);
+      result.setContent("<?xml version=\"1.0\" encoding=\"UTF-8\" ?><info deviceID=\"C8DF84AE0B6E\"><name>SoundTouch 20</name><type>SoundTouch 20</type" + "><margeAccountUUID" +
+          ">6990307</margeAccountUUID><components><component><componentCategory>SCM</componentCategory><softwareVersion>19.0.5.42017.2794643 " + "epdbuild.trunk.cepeswbld02" +
+          ".2018-04-25T18:23:30</softwareVersion><serialNumber>F8124895404720048620440</serialNumber></component><component><componentCategory>" + "PackagedProduct" +
+          "</componentCategory><serialNumber>069428P81639976AE</serialNumber></component></components><margeURL>https://streaming.bose.com</margeURL>" + "<networkInfo " +
+          "type=\"SCM\"><macAddress>C8DF84AE0B6E</macAddress><ipAddress>192.168.178.61</ipAddress></networkInfo><networkInfo " + "type=\"SMSC\"><macAddress>C8DF84615084" +
+          "</macAddress><ipAddress>192.168.178" + ".61</ipAddress></networkInfo><moduleType>sm2</moduleType><variant>spotty</variant><variantMode>normal</variantMode" +
+          "><countryCode>GB</countryCode><regionCode>GB" + "</regionCode></info>");
+      return result;
+    }
+  };
+
+  private static final MockLowLevelHttpRequest INFO_MOCK_VERSION_BROKEN = new MockLowLevelHttpRequest() {
+    @Override
+    public LowLevelHttpResponse execute() {
+      final MockLowLevelHttpResponse result = new MockLowLevelHttpResponse();
+      result.setContentType(Xml.MEDIA_TYPE);
+      result.setContent("<?xml version=\"1.0\" encoding=\"UTF-8\" ?><info deviceID=\"C8DF84AE0B6E\"><name>SoundTouch 10</name><type>SoundTouch 10</type" + "><margeAccountUUID" +
+          ">6990307</margeAccountUUID><components><component><componentCategory>SCM</componentCategory><softwareVersion>1x.0.5.42017.2794643 " + "epdbuild.trunk.cepeswbld02" +
+          ".2018-04-25T18:23:30</softwareVersion><serialNumber>F8124895404720048620440</serialNumber></component><component><componentCategory>" + "PackagedProduct" +
+          "</componentCategory><serialNumber>069428P81639976AE</serialNumber></component></components><margeURL>https://streaming.bose.com</margeURL>" + "<networkInfo " +
+          "type=\"SCM\"><macAddress>C8DF84AE0B6E</macAddress><ipAddress>192.168.178.61</ipAddress></networkInfo><networkInfo " + "type=\"SMSC\"><macAddress>C8DF84615084" +
+          "</macAddress><ipAddress>192.168.178" + ".61</ipAddress></networkInfo><moduleType>sm2</moduleType><variant>spotty</variant><variantMode>normal</variantMode" +
+          "><countryCode>GB</countryCode><regionCode>GB" + "</regionCode></info>");
+      return result;
+    }
+  };
+
+  private static final MockLowLevelHttpRequest INFO_MOCK_NO_COMPONENTS = new MockLowLevelHttpRequest() {
+    @Override
+    public LowLevelHttpResponse execute() {
+      final MockLowLevelHttpResponse result = new MockLowLevelHttpResponse();
+      result.setContentType(Xml.MEDIA_TYPE);
+      result.setContent("<?xml version=\"1.0\" encoding=\"UTF-8\" ?><info deviceID=\"C8DF84AE0B6E\"><name>SoundTouch 10</name><type>SoundTouch 10</type" + "><margeAccountUUID" +
+          ">6990307</margeAccountUUID><components /><margeURL>https://streaming.bose.com</margeURL><networkInfo type=\"SCM\"><macAddress>C8DF84AE0B6E</macAddress><ipAddress>192" +
+          ".168.178.61</ipAddress></networkInfo><networkInfo " + "type=\"SMSC\"><macAddress>C8DF84615084</macAddress><ipAddress>192.168.178" + ".61</ipAddress></networkInfo" +
+          "><moduleType>sm2</moduleType><variant>spotty</variant><variantMode>normal</variantMode><countryCode>GB</countryCode><regionCode>GB" + "</regionCode></info>");
+      return result;
+    }
+  };
+
   public void test01_getGroup() {
     LOGGER.info("test01_getGroup started");
     final HttpTransport transport = new MockHttpTransport() {
       @Override
       public LowLevelHttpRequest buildRequest(final String method, final String url) {
-        return new MockLowLevelHttpRequest() {
-          @Override
-          public LowLevelHttpResponse execute() {
-            final MockLowLevelHttpResponse result = new MockLowLevelHttpResponse();
-            result.setContentType(Xml.MEDIA_TYPE);
-            result.setContent("<?xml version=\"1.0\" encoding=\"UTF-8\" ?><group id=\"$STRING\"><name>name</name><masterDeviceId>masterDeviceId</masterDeviceId><roles><groupRole" +
-                "><deviceId>deviceId1</deviceId><role>LEFT1</role><ipAddress>ipAddress1</ipAddress></groupRole><groupRole><deviceId>deviceId2</deviceId><role>RIGHT2</role" +
-                "><ipAddress>ipAddress2</ipAddress></groupRole></roles><senderIPAddress>senderIPAddress</senderIPAddress><status>status</status></group>");
-            return result;
-          }
-        };
+        if (url.endsWith("/info")) {
+          return INFO_MOCK_OK;
+        } else {
+          return GROUP_MOCK_OK;
+        }
       }
     };
 
@@ -83,16 +161,11 @@ public class GroupTest extends TestCase {
     final HttpTransport transport = new MockHttpTransport() {
       @Override
       public LowLevelHttpRequest buildRequest(final String method, final String url) {
-        return new MockLowLevelHttpRequest() {
-          @Override
-          public LowLevelHttpResponse execute() {
-            final MockLowLevelHttpResponse result = new MockLowLevelHttpResponse();
-            result.setContentType(Xml.MEDIA_TYPE);
-            result.setContent("<?xml version=\"1.0\" encoding=\"UTF-8\" ?><group id=\"$STRING\"><name>name</name><masterDeviceId>masterDeviceId</masterDeviceId><roles" +
-                "/><senderIPAddress>senderIPAddress</senderIPAddress><status>status</status></group>");
-            return result;
-          }
-        };
+        if (url.endsWith("/info")) {
+          return INFO_MOCK_OK;
+        } else {
+          return GROUP_MOCK_EMPTY;
+        }
       }
     };
 
@@ -114,4 +187,77 @@ public class GroupTest extends TestCase {
     }
     LOGGER.info("test02_getGroupEmptyList started");
   }
+
+  public void test03_getGroupFailDueToST20() {
+    LOGGER.info("test03_getGroupFailDueToST20 started");
+    final HttpTransport transport = new MockHttpTransport() {
+      @Override
+      public LowLevelHttpRequest buildRequest(final String method, final String url) {
+        if (url.endsWith("/info")) {
+          return INFO_MOCK_ST20;
+        } else {
+          return GROUP_MOCK_OK;
+        }
+      }
+    };
+
+    final SoundTouch soundTouchApi = new SoundTouchApi(HttpTesting.SIMPLE_GENERIC_URL.toURL(), transport);
+    try {
+      soundTouchApi.getGroupApi()
+          .getGroup();
+      Assert.fail();
+    } catch (final SoundTouchApiException e) {
+      assertEquals("/group Call Requires SoundTouch 10, but this is 'SoundTouch 20'", e.getMessage());
+    }
+    LOGGER.info("test03_getGroupFailDueToST20 started");
+  }
+
+  public void test03_getGroupFailDueToBrokenVersion() {
+    LOGGER.info("test03_getGroupFailDueToBrokenVersion started");
+    final HttpTransport transport = new MockHttpTransport() {
+      @Override
+      public LowLevelHttpRequest buildRequest(final String method, final String url) {
+        if (url.endsWith("/info")) {
+          return INFO_MOCK_VERSION_BROKEN;
+        } else {
+          return GROUP_MOCK_OK;
+        }
+      }
+    };
+
+    final SoundTouch soundTouchApi = new SoundTouchApi(HttpTesting.SIMPLE_GENERIC_URL.toURL(), transport);
+    try {
+      soundTouchApi.getGroupApi()
+          .getGroup();
+      Assert.fail();
+    } catch (final SoundTouchApiException e) {
+      assertEquals("Unable to parse Version String '1x.0.5.42017.2794643 epdbuild.trunk.cepeswbld02.2018-04-25T18:23:30'", e.getMessage());
+    }
+    LOGGER.info("test03_getGroupFailDueToBrokenVersion started");
+  }
+
+  public void test04_getGroupFailDueToNoComponents() {
+    LOGGER.info("test04_getGroupFailDueToNoComponents started");
+    final HttpTransport transport = new MockHttpTransport() {
+      @Override
+      public LowLevelHttpRequest buildRequest(final String method, final String url) {
+        if (url.endsWith("/info")) {
+          return INFO_MOCK_NO_COMPONENTS;
+        } else {
+          return GROUP_MOCK_OK;
+        }
+      }
+    };
+
+    final SoundTouch soundTouchApi = new SoundTouchApi(HttpTesting.SIMPLE_GENERIC_URL.toURL(), transport);
+    try {
+      soundTouchApi.getGroupApi()
+          .getGroup();
+      Assert.fail();
+    } catch (final SoundTouchApiException e) {
+      assertEquals("Unable to fetch Version of SoundTouch", e.getMessage());
+    }
+    LOGGER.info("test04_getGroupFailDueToNoComponents started");
+  }
+
 }
