e3d5ed7dfe2337e8c22e9606538b1cdb84420561
Gerald Madlmayr <gerald.madlmayr@gmx.at>
2018-10-21 12:49:34 +0200

    Update Tests to fix Code Coverage (#22)

--- src/test/java/org/soundtouch4j/PresetsApiTest.java
+++ src/test/java/org/soundtouch4j/PresetsApiTest.java
@@ -24,19 +24,6 @@ public class PresetsApiTest {
     final PresetResponse response = soundTouchApi.getPresetApi()
         .getPresets();
 
-    assertEquals("PresetResponse{presetList=[Preset{id=1, createdOn=Sat Sep 08 19:02:48 CEST 2018, updatedOn=Sat Sep 08 19:02:48 CEST 2018, " +
-        "contentItem=ContentItem{source=INTERNET_RADIO, isPresetable=true, location='18298', sourceAccount='', itemName='ORF Hitradio Ö3', containerArt='http://item.radio456" +
-        ".com/007452/logo/logo-18298.jpg'}}, Preset{id=2, createdOn=Sat Sep 08 19:03:00 CEST 2018, updatedOn=Sat Sep 08 21:48:55 CEST 2018, " +
-        "contentItem=ContentItem{source=INTERNET_RADIO, isPresetable=true, location='80709', sourceAccount='', itemName='1 Pure EDM Radio', containerArt='http://item.radio456" +
-        ".com/007452/logo/logo-80709.jpg'}}, Preset{id=3, createdOn=Sat Sep 08 19:03:39 CEST 2018, updatedOn=Sat Sep 08 19:03:39 CEST 2018, " +
-        "contentItem=ContentItem{source=INTERNET_RADIO, isPresetable=true, location='8992', sourceAccount='', itemName='ENERGY 93.3 München', containerArt='http://item.radio456" +
-        ".com/007452/logo/logo-8992.jpg'}}, Preset{id=4, createdOn=Sat Sep 08 19:03:17 CEST 2018, updatedOn=Sat Sep 08 19:03:17 CEST 2018, " +
-        "contentItem=ContentItem{source=INTERNET_RADIO, isPresetable=true, location='3687', sourceAccount='', itemName='95.5 Charivari', containerArt='http://item.radio456" +
-        ".com/007452/logo/logo-3687.jpg'}}, Preset{id=5, createdOn=Sat Sep 08 19:22:08 CEST 2018, updatedOn=Sat Sep 08 19:22:08 CEST 2018, " +
-        "contentItem=ContentItem{source=INTERNET_RADIO, isPresetable=true, location='76484', sourceAccount='', itemName='Costa Del Mar - DeepHouse', containerArt='http://item" +
-        ".radio456.com/007452/logo/logo-76484.jpg'}}, Preset{id=6, createdOn=Sat Sep 08 19:23:51 CEST 2018, updatedOn=Sat Sep 08 19:23:51 CEST 2018, " +
-        "contentItem=ContentItem{source=INTERNET_RADIO, isPresetable=true, location='45289', sourceAccount='', itemName='Deep House Lounge', containerArt='http://item.radio456" +
-        ".com/007452/logo/logo-45289.jpg'}}]}", response.toString());
     assertEquals(6, response.getPresetList()
         .size());
     assertEquals(1, response.getPresetList()
